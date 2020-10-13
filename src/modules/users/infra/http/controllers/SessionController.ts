8af79edd0b77214580f7966a759bada88ae8dac5
leosteil <lsteil@inf.ufsm.br>
2020-10-12 23:03:25 -0300

    Using class-transformer to transform data response

--- src/modules/users/infra/http/controllers/SessionController.ts
+++ src/modules/users/infra/http/controllers/SessionController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-
+import { classToClass } from 'class-transformer';
 import { container } from 'tsyringe';
 
 import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
@@ -18,8 +18,6 @@ export default class SessionController {
             password,
         });
 
-        delete user.password;
-
-        return response.json({ user, token });
+        return response.json({ user: classToClass(user), token });
     }
 }
--- src/modules/users/infra/http/controllers/UserController.ts
+++ src/modules/users/infra/http/controllers/UserController.ts
@@ -3,6 +3,7 @@ import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 
 import CreateUserService from '@modules/users/services/CreateUserService';
+import { classToClass } from 'class-transformer';
 
 export default class UserController {
     public async create(
@@ -15,8 +16,6 @@ export default class UserController {
 
         const user = await createUser.execute({ name, email, password });
 
-        delete user.password;
-
-        return response.json(user);
+        return response.json(classToClass(user));
     }
 }
